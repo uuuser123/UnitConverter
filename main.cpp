07324a95b5419a5edb8a47c0a6b253c0ed4b39e2
rzy <2571476073@qq.com>
2018-06-05 18:11:17 +0800

    revise framework

--- main.cpp
+++ main.cpp
@@ -1,37 +1,38 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
 class Unit{
 	public:
-		int show() const;
-		Unit(double _val,string _symbol):val(_val),symbol(_symbol){}
+		int Show() const;
+		Unit(double _val, string _symbol):val(_val),symbol(_symbol){}
 		~Unit(){}
-		Unit operator + (const Unit &add){}
-		Unit operator - (const Unit &add){}
-		Unit operator * (const Unit &add){}
-		Unit operator / (const Unit &add){}
-		Unit operator > (const Unit &add){}
-		Unit operator < (const Unit &add){}
-		Unit operator == (const Unit &add){}
-	private:
+	protected:
 		double val;
 		string symbol;
 };
 
+int Unit::Show() const
+{
+	printf("%lf%s\n",val,&symbol[0]);
+	return 0;
+}
+
 class Weight:public Unit{
 	public:
-		Weight(int _val,string _symbol,double _coefficient):Unit(_val,_symbol),coefficient(_coefficient){}
+		Weight(int _val,string _symbol):Unit(_val,_symbol){coefficient=0.1;}
 		~Weight(){}
-		
+		Weight operator + (const Weight &add){return Weight(val+add.val,symbol);}
 	private:
 		double coefficient;
 };
 
 
 int main(){
-	Weight a(1,"1",1);
-	Weight b(2,"2",2);
+	Weight a(1,"kg");
+	Weight b(2,"kg");
 	Weight c = a+b;
+	c.Show();
 	return 0;
 }
 
