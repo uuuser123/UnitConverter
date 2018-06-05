961af811d3eb1c753c6d4205ac4edcd68832babd
rzy <2571476073@qq.com>
2018-06-05 21:26:08 +0800

    add operator and unitTypeName

--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 
 using namespace std;
 
+string unitTypeName[0]={};
 string unitName[0][0]={};
 double coefficient[0][0]={};
 
 class unit{
 	public:
-		unit(){}
+		unit(string unitStr){}//parse the unitId and the unitType
 		~unit(){}
 		double convert(string newUnit);//change the unit and return coefficient(if the unit type is different return -1)
 		int set(string newUnit);//change the unit and return 0 if suceed(only use if the unit type is diffierent)
 		string showUnit();//return the unit name
+		string showUnitTypeName();//return the unit type name
 	private:
 		int unitId;
 		int unitType;
@@ -19,9 +21,19 @@ class unit{
 
 class unitNum:unit{
 	public:
+		unitNum(string unitNumStr):unit(unitNumStr){}//parse the value
 		int showVal();//return the value of the unitNum
 		int change(string A);//change the val and the unit
+		//support the operator
 		bool operator ==(const unitNum &A);//return true if both unit and val equal
+		bool operator <(const unitNum &A);
+		bool operator >(const unitNum &A);
+		bool operator <=(const unitNum &A);
+		bool operator >=(const unitNum &A);
+		unitNum operator + (const unitNum &A);
+		unitNum operator - (const unitNum &A);
+		unitNum operator * (const unitNum &A);
+		unitNum operator / (const unitNum &A);
 	private:
 		double val;
 };
