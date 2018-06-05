085cacf8864874c4fb5c021c4212ac0e48cdbfe7
cxh123 <931736813@qq.com>
2018-06-05 17:47:24 +0800

    basic framework

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Unit{
 	public:
 		int show() const;
-		Unit(){}
+		Unit(double _val,string _symbol):val(_val),symbol(_symbol){}
 		~Unit(){}
+		Unit operator + (const Unit &add){}
+		Unit operator - (const Unit &add){}
+		Unit operator * (const Unit &add){}
+		Unit operator / (const Unit &add){}
+		Unit operator > (const Unit &add){}
+		Unit operator < (const Unit &add){}
+		Unit operator == (const Unit &add){}
 	private:
-		int val;
+		double val;
 		string symbol;
 };
 
 class Weight:public Unit{
 	public:
+		Weight(int _val,string _symbol,double _coefficient):Unit(_val,_symbol),coefficient(_coefficient){}
+		~Weight(){}
+		
 	private:
+		double coefficient;
+};
+
+
+int main(){
+	Weight a(1,"1",1);
+	Weight b(2,"2",2);
+	Weight c = a+b;
+	return 0;
 }
 
